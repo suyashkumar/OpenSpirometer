db163ff60b64a001b63f8d86f69144a7940a5419
Suyash Kumar <suyash.kumar@duke.edu>
2016-01-07 23:39:58 -0500

    Frontend angular app comment update

--- Server/static/js/core.js
+++ Server/static/js/core.js
@@ -5,6 +5,7 @@
  */
 var spiroApp = angular.module('spiroapp', ['ngRoute']);
 
+// Configure "single" page angular app using ngRoute
 spiroApp.config(function($routeProvider){
 	$routeProvider.when('/',{
 		templateUrl : 'pages/home.html',
@@ -15,31 +16,38 @@ spiroApp.config(function($routeProvider){
 		controller : 'dashboardController'
 	});
 
-});
+}); 
 
+// Set up Controllers (move controller bodies to different files later):
 
-
-// Set up Controllers:
+// Main log-in screen controller:
 spiroApp.controller("mainController", function($scope,$http){
-	$scope.goToDash = function(){
-		console.log("user: "+$scope.username); 
-		window.location='#/dash/'+$scope.username;
+	$scope.goToDash = function(){ 
+		window.location='#/dash/'+$scope.username; // Redirects to the proper dash page
 	} 
  }); 
+
+// Dashboard controller:
 spiroApp.controller("dashboardController", function($scope, $http, $routeParams){
-	$scope.username=$routeParams.user;
-	if ($scope.username=="") window.location='#/';
-	$http.get('/api/'+$scope.username+"/data").success(function(data){
-		// Get data
-	makeOverviewDygraph(data);
-	$scope.spiroData=[];
-	for (i=0;i<data.length;i++){
-		var t = new Date (parseInt(data[i].date)*1000); 
-		$scope.spiroData.push({date: t.getMonth().toString()+"/"+t.getDate().toString()+"/"+t.getFullYear().toString(), ratio: (data[i].FEV/data[i].FVC).toFixed(3), tags: data[i].params.tags.toString()}); 
-	}
+	$scope.username=$routeParams.user; // Get Username
+	if ($scope.username=="") window.location='#/'; // Redirect to main page if no username
+	// Get data for the user from the server:
+	$http.get('/api/'+$scope.username+"/data").success(function(data){ 
+		makeOverviewDygraph(data); // Update the graph
+		$scope.spiroData=[]; // Holds data used by the table
+		for (i=0;i<data.length;i++){ // Populate data for the table
+			var t = new Date (parseInt(data[i].date)*1000); 
+			$scope.spiroData.push({date: t.getMonth().toString()+"/"+t.getDate().toString()+"/"+t.getFullYear().toString(), ratio: (data[i].FEV/data[i].FVC).toFixed(3), tags: data[i].params.tags.toString()}); 
+		}
 	});
 });
 
+/*
+ * makeOverviewDygraph()
+ * Takes data returned from the server, formats it, and 
+ * creates a graph using dygraph.js. The graph is inserted into the 
+ * div with id=graphdiv.
+ */
 var makeOverviewDygraph = function(data){
 	var dataArray=[];
 	for(i=0;i<data.length;i++){
